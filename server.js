bf8336dec1677a92fdaa217c2c40def50510597d
Cse_academia <62127870+CSEacademia@users.noreply.github.com>
2020-10-02 13:46:27 +0600

    final

--- server.js
+++ server.js
@@ -51,7 +51,9 @@ app.use(require("./routes/teachers"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client','build','index.html'))
+})
 }
 
 app.listen(port, () => {
